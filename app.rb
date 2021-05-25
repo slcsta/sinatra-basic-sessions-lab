e59815effe3660a58f0934e3e9345bb239fdd3d4
Sarah <sarahlchavez@gmail.com>
2021-05-25 05:36:23 -0700

    commit tree 292140fd6a868f85234fb4bcce65a61ab077a685

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+    # In Sinatra we enable sessions within the controller (e.g. app.rb)
+    # by adding two lines in the configure block
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :'checkout'
+    end
 end
